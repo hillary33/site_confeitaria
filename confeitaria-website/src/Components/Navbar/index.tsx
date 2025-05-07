e1389eb06a3f2f4e213c6bf3787b75d222ea4124
hillary33 <hillary.ferreir@gmail.com>
2025-05-07 15:43:31 -0300

    feat: implement Navbar component with styles and layout

--- confeitaria-website/src/Components/Navbar/index.tsx
+++ confeitaria-website/src/Components/Navbar/index.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import styles from './Navbar.module.css'
 
 export default function Navbar() {
     return (
-        <>
-            <span>Emy's</span>
-            <nav>
+        <div className={styles.navbar}>
+            <nav className={styles.menu}>
                <Link to='/'>Home</Link> 
             </nav>
-        </>
+            <span className={styles.logo}>Emy's</span>
+        </div>
     )
 }
